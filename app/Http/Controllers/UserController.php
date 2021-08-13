e2b3a741e202cb626bb08206731cda5484fc0da8
Anh Tuấn Cao <72505667+AnhtuanCaoo@users.noreply.github.com>
2021-08-13 15:00:48 +0700

    edit blade files

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users =User::latest()->paginate(5);
+        return view('userLayouts.app',compact('users'))->with('i',(request()->input('page',1)-1)*5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     */
+  
+    /**
+     * Store a newly created resource in storage.
+     *
+     */
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return view('userLayouts.show',compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     */
+
+    /**
+     * Update the specified resource in storage.
+     *
+     */
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     *=
+     */
+    
 }
--- resources/views/userLayouts/app.blade.php
+++ resources/views/userLayouts/app.blade.php
@@ -16,45 +16,26 @@
               <td>Name</td>
               <td>Number</td>
               <td>Email</td>
-              <td>Sell Price</td>
                 <th width="120px">Action</th>
             </tr>
-            @foreach ($juices as $juice)
+            @foreach ($users as $user)
                 <tr>
                     <td>{{ ++$i }}</td>
-                    <td>{{ $juice->juicename }}</td>
-                    <td>{{ $juice->number }}</td>
-                    <td><img src="{{ $juice->image }}" alt="" style="width:100px;height:100px;"></td>
-                    <td>{{ $juice->nicotin }}</td>
-                    <td>{{ $juice->flavor }}</td>
-                    <td>{{ $juice->brand }}</td>
-                    <td>{{ $juice->price }}</td>
-                    <td>{{ $juice->sellingprice}}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->number }}</td>
+                    <td>{{ $user->email }}</td>
                     <td>
-                        <form action="{{ route('juices.destroy', $juice->id) }}" method="POST">
+                        
     
-                            <a href="{{ route('juices.show', $juice->id) }}" title="show">
+                            <a href="{{ route('users.show', $user->id) }}" title="show">
                                 <i class="fas fa-eye text-success  fa-lg"></i>
                             </a>
     
-                            <a href="{{ route('juices.edit', $juice->id) }}">
-                                <i class="fas fa-edit  fa-lg"></i>
-    
-                            </a>
-    
-                            @csrf
-                            @method('DELETE')
-    
-                            <button type="submit" title="delete" style="border: none; background-color:transparent;">
-                                <i class="fas fa-trash fa-lg text-danger"></i>
-    
-                            </button>
-                        </form>
                     </td>
                 </tr>
             @endforeach
         </table>          
     </div>
-    {!! $juices->links() !!}  
+    {!! $users->links() !!}  
 
 @endsection
